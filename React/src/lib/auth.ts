a4785de069ffe77f97cbd2538f25cf234a4f5423
Alan-CF <alanyaco.cf@gmail.com>
2026-03-12 10:27:05 -0600

    [ADD] Redirect Config

--- React/src/lib/auth.ts
+++ React/src/lib/auth.ts
@@ -3,6 +3,9 @@ import { supabase } from './supabaseClient'
 export async function signInWithGoogle() {
   return await supabase.auth.signInWithOAuth({
     provider: 'google',
+    options: {
+      redirectTo: `${window.location.origin}/`,
+    },
   })
 }
 
