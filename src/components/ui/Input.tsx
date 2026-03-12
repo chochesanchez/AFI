0bec20c0e969729b9c2ced773673671750868d9d
Alan-CF <alanyaco.cf@gmail.com>
2026-03-11 19:38:52 -0600

    [ADD] Dialog and Input

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,13 +1,17 @@
 interface InputProps {
     placeholder?: string;
     className?: string;
+    value?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function Input({ placeholder, className }: InputProps) {
+function Input({ placeholder, className, value, onChange }: InputProps) {
     return (
         <input
         type="text"
         placeholder={placeholder}
+        value={value}
+        onChange={onChange}
         className={`border-4 border-secondary focus:border-primary focus:outline-none rounded-2xl p-4 font-lato transition-colors ${className ?? ""}`}
         />
     );
